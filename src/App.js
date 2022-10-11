a0d1da43e543f9a0cb31d375277e92e7155a00ec
ykcsoftwarer <yusufkucukcopur@gmail.com>
2022-10-11 18:34:15 +0300

    data

--- src/App.js
+++ src/App.js
@@ -19,8 +19,6 @@ function App() {
         </a>
       </header>
     </div>
-
-    
   );
 }
 
---
+++ src/assests/data.jsx
@@ -0,0 +1,41 @@
+const data = [
+    {
+        id:1,
+        title:"Software Development",
+        img:"https://clarusway.com/wp-content/uploads/elementor/thumbs/v1-Fullstack-developer-prp8n1n0eh117378xwocaxaqg0o2yn1zo47k26req0.jpg",
+        desc:"Become a software and application developer, well-equipped with both front-end and back-end."
+    },
+    {
+        id:2,
+        title:"Cloud & Devops Engineering",
+        img:"https://clarusway.com/wp-content/uploads/elementor/thumbs/v1-aws-solution-architechdevops-engineering-prp8n4giyz4w5x35hfw80el486a6lqd6oi60i0n87c.jpg",
+        desc:"We prepare you for AWS DevOps certifications. Our graduates land the best IT jobs!"
+    },
+    {
+        id:3,
+        title:"Data Science",
+        img:"https://clarusway.com/wp-content/uploads/elementor/thumbs/Data-Science-prp8qd0iifkg5kduku9yll69vybp477wgj280buers.jpg",
+        desc:"Learn Data Science, extract from different data types, build complex algorithms, visualization."
+    },
+    {
+        id:4,
+        title:"Cyber Security",
+        img:"https://clarusway.com/wp-content/uploads/elementor/thumbs/Cyber2-1-prp8pxz3h2zuzszp0nrxhoywdsdtp1k72gmgbwgpjc.jpg",
+        desc:"Learn Cyber Security Fundamentals and continue with advanced topics. No IT background is required for this high-demand career!"
+    },
+    {
+        id:5,
+        title:"Salesforce",
+        img:"https://clarusway.com/wp-content/uploads/elementor/thumbs/v1-Salesforce_-prp8mythtyx689bcedgglg0cnv1zbjqsnq93mcvl8o.jpg",
+        desc:"Learn to become a Salesforce Administrator or Developer. No IT background is required for this high-demand career!"
+    },
+    {
+        id:6,
+        title:"Web3-Blockchain",
+        img:"https://clarusway.com/wp-content/uploads/elementor/thumbs/Blockchains-prp8pywxnx15beybv66k26qcz696wqnxel9xt6fbd4.jpg",
+        desc:"Learn Web 3.0 and Blockchain concepts – build applications on the Blockchain."
+    }
+    
+]
+
+export default data
